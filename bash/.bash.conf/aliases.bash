a62b0ac5a31b800076d10f4f5083871672e0071e
Stefan <stadolf@gmail.com>
2017-10-13 18:55:42 +0100

    slight updates of bash aliases

--- bash/.bash.conf/aliases.bash
+++ bash/.bash.conf/aliases.bash
@@ -1,8 +1,7 @@
 alias ll="ls -la"
 alias ..="cd .."
 alias ...="cd ../.."
-alias gop="cd /usr/local/apache2/htdocs"
-alias tailf="tail -f /logs/access.log"
+alias gop="cd ~/symfony"
 alias sf="bin/console"
 
 mkd() { mkdir -p "$1" && cd "$1"; }
