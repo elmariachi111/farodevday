151075d585a1d8f02d5b2e539c999de09d93d265
Stefan <stadolf@gmail.com>
2017-10-17 14:26:32 +0100

    use .env files to control docker-compose secrets, application: handlebars, common logging transports, minimal "frontend" for posting messages slightly better debug config

--- rsyslog/README.md
+++ rsyslog/README.md
@@ -1,1 +1,9 @@
-docker-compose up
+`docker-compose up` starts three machines:
+
+* the official loggly Docker logger image that you can send logs towards (514/udp)
+* a mysql instance
+* the application that connects to mysql
+
+to test logging to loggly yourself you must [create a loggly account](https://www.loggly.com/signup/) and get a [customer token](https://your-account.loggly.com/tokens). Then create an `.env` file by copying `.env.dist` and set the token there. It's picked up by the logger service automatically.
+
+The application is bound to your local port 8080 and will log http requests to your loggly account. The source code is mounted to the application service's container so if you reflect any changes to the source code you have to restart the running application (`docker-compose restart application`).
--- rsyslog/src/index.js
+++ rsyslog/src/index.js
@@ -3,36 +3,56 @@ const winston = require('winston')
 const winstonExpress = require('express-winston');
 const winstonSyslog = require('winston-syslog').Syslog;
 const mysql = require('mysql')
+const exphbs  = require('express-handlebars');
 const redis = require('redis')
 const bodyParser = require('body-parser')
 
-const app = express()
 const con = mysql.createConnection(process.env.MYSQL_URL);
 con.connect()
 
-app.use(winstonExpress.logger({
-  transports: [
-    new winston.transports.Syslog({
-      host: 'logger',
-      protocol: 'udp4',
-      port: 514
-    })
-  ]
+const app = express()
+
+const syslogTransport = new winston.transports.Syslog({
+  host: 'logger',
+  protocol: 'udp4',
+  port: 514
+});
+
+const logger = new (winston.Logger)({
+  transports: [ syslogTransport ]
+});
+
+app.use(winstonExpress.logger({ //logs all requests
+  transports: [ syslogTransport ]
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.engine('handlebars', exphbs.create({
+  helpers: {
+    utc: d => { return d.toUTCString(); }
+  }
+}).engine); //handlebars
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  logger.info("homepage");
+  con.query('SELECT * FROM messages ', (error, results, fields) => {
+    res.render('index', {
+      messages: results
+    });
+  });
 })
 
 app.post('/msg', (req, res) => {
-  const message = { message: req.body.message};
+  const message = { message: req.body.message };
   const query = con.query(
     'INSERT INTO messages SET ?', message, (error, results, fields) => {
-      res.send(200);
+      logger.info("message created: " + message.message);
       if (error) throw error;
+      res.redirect("/");
   });
 })
 
@@ -41,7 +61,7 @@ app.get('/exc', (req, res, next) => {
 });
 
 app.use(function (err, req, res, next) {
-    console.error(err.stack)
+    logger.error(err.stack);
     res.status(500).send('Something broke!')
 })
   
